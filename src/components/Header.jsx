fdb0263275edb0c07a92df403e2ce954dcade4a6
Seungyeon Oh <alstmd9902@naver.com>
2026-01-02 14:45:19 +0900

    refactor: tialwindcss 변환

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center px-2.5 py-5 bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
+      <ul className="flex justify-center items-center gap-5 [&>li]:text-[13px]">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
